e694ccf64022a47d142babc43a7dca0a030747a0
Jan Zajda <j.zajda47@gmail.com>
2022-09-01 21:18:45 +0200

    fix cors policy error

--- expenses-manager-backend/src/main/java/project/zti/expensesmanagerbackend/ExpensesManagerBackendApplication.java
+++ expenses-manager-backend/src/main/java/project/zti/expensesmanagerbackend/ExpensesManagerBackendApplication.java
@@ -19,7 +19,7 @@ public class ExpensesManagerBackendApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").allowedMethods("*").allowedHeaders("*")
-                        .allowedOrigins("https://expenses-app-zti-front.herokuapp.com");
+                        .allowedOrigins("https://expenses-manger-backend.herokuapp.com/");
             }
         };
     }
