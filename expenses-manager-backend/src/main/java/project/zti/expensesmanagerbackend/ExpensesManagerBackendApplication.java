b4979660739006058ddc381a8f86e13358bd6029
Jan Zajda <j.zajda47@gmail.com>
2022-09-01 21:15:23 +0200

    configure cors policy

--- expenses-manager-backend/src/main/java/project/zti/expensesmanagerbackend/ExpensesManagerBackendApplication.java
+++ expenses-manager-backend/src/main/java/project/zti/expensesmanagerbackend/ExpensesManagerBackendApplication.java
@@ -2,6 +2,9 @@ package project.zti.expensesmanagerbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ExpensesManagerBackendApplication {
@@ -10,4 +13,14 @@ public class ExpensesManagerBackendApplication {
         SpringApplication.run(ExpensesManagerBackendApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedHeaders("*")
+                        .allowedOrigins("https://expenses-app-zti-front.herokuapp.com");
+            }
+        };
+    }
 }
